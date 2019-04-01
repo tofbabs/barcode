050a9e4e2f5186d48811be3dfb60465ddd4898ac
Tofunmi <tofunmibabatunde@gmail.com>
2019-04-01 19:51:27 +0100

    Update: Download e-Bib, format email

--- app/Http/Controllers/BarcodeController.php
+++ app/Http/Controllers/BarcodeController.php
@@ -14,33 +14,29 @@ use Mail;
 
 class BarcodeController extends Controller
 {
-	public function index() {
-		
-
-		$data = ['barcode' => 12345];
-
-		$pdf = PDF::loadView('welcome', $data);
-
-		Mail::send('emails.test', $data, function($message) use($pdf)
-		{
-		    $message->from('eko.5samuel@gmail.com', 'Your Name');
-
-		    $message->to('eko.5samuel@gmail.com')->subject('Invoice');
-
-		    $message->attachData($pdf->output(), "invoice.pdf");
-		});
+    public function index()
+    {
 
 
-	}
+        $data = ['barcode' => 12345];
 
-	public function test()
-	{
-	    $barcode = 22345;
-		$pdf = PDF::loadView('pdf.ebib', ['barcode' => $barcode])
+        $pdf = PDF::loadView('welcome', $data);
+
+        Mail::send('emails.test', $data, function ($message) use ($pdf) {
+            $message->from('eko.5samuel@gmail.com', 'Your Name');
+            $message->to('eko.5samuel@gmail.com')->subject('Invoice');
+            $message->attachData($pdf->output(), "invoice.pdf");
+        });
+    }
+
+    public function test()
+    {
+        $barcode = 22345;
+        $pdf = PDF::loadView('pdf.ebib', ['barcode' => $barcode])
             ->setPaper('a5', 'landscape')
-            ->save(storage_path('app/public/pdf/'.$barcode.'.pdf'));
-		return $pdf->stream('invoice.pdf');
-	}
+            ->save(storage_path('app/public/pdf/' . $barcode . '.pdf'));
+        return $pdf->stream('invoice.pdf');
+    }
 
     public function show()
     {
@@ -54,41 +50,44 @@ class BarcodeController extends Controller
             ], 422);
         }
 
+        $email = request()->email;
+        $name = request()->name;
+
+
+
         try {
-            $email = request()->email;
-            if(Barcode::whereEmail($email)->exists())
-            {
-                $barcodes = Barcode::whereEmail($email)->first();
-                $barcode = $barcodes->ebib;
-                $email = $barcodes->email;
-                if (request()->isRequestEmail === 1)
-                {
-                    $this->sendUserEbibPDF($email, $barcode);
-                    $this->sendAdminEbibPDF($email, $barcode);
-                    return response()->json([
-                        'data' => $barcodes,
-                    ], 200);
-                } else {
-                    return response()->json([
-                        'data' => $barcodes->barcode
-                    ], 200);
-                }
-            } else {
-                $ebib = $this->generateID();
-                $data['email'] = request()->email;
-                $data['ebib'] = $ebib;
-                $data['barcode'] = 'app/public/pdf/'.$ebib.'.pdf';
-                if($barcodes = Barcode::create($data)){
-                    $email = $barcodes->email;
+
+            if (Barcode::whereEmail($email)->exists()) {
+                    $barcodes = Barcode::whereEmail($email)->first();
                     $barcode = $barcodes->ebib;
-                    $this->getStoredPDF($barcode);
-                    $this->sendUserEbibPDF($email, $barcode);
+                    $email = $barcodes->email;
+                    if (request()->isRequestEmail === 1) {
+                        $this->sendUserEbibPDF($email, $barcode, $name);
+                        $this->sendAdminEbibPDF($email, $barcode);
+                    }
+                } else {
+                $ebib = $this->generateID();
+                $barcode = 'app/public/pdf/'.$ebib.'.pdf';
+
+                $data = compact("email", "ebib", $barcode);
+
+                if ($barcodes = Barcode::create($data)) {
+                    $barcode = $barcodes->ebib;
+
+                    // Stores barcode to PDF 
+                    $this->storePDF($barcode);
+
+                    $this->sendUserEbibPDF($email, $barcode, $name);
                     $this->sendAdminEbibPDF($email, $barcode);
-                    return response()->json([
-                        'data' => $barcodes,
-                    ], 200);
+
                 }
             }
+
+            $pdf = PDF::loadView('pdf.ebib', ['barcode' => $barcode])
+                ->setPaper('a5', 'landscape');
+
+            return response()->download($pdf->output);
+
         } catch (\Exception $e) {
             return response()->json([
                 'errors' => $e->getMessage(),
@@ -96,90 +95,86 @@ class BarcodeController extends Controller
         }
     }
 
-    public function sendUserEbibPDF($email, $barcode)
+    public function sendUserEbibPDF($email, $barcode, $name)
     {
-    	$data = ['barcode' => $barcode];
+        $data = ['barcode' => $barcode, 'name' => $name];
+        $pdf = PDF::loadView('pdf.ebib', $data)
+            ->setPaper('a5', 'landscape');
 
-		$pdf = PDF::loadView('pdf.ebib', $data)
-                    ->setPaper('a5', 'landscape');
+        $subject = "Hello $name, your BIB Number for the Access Bank Lagos City Marathon 2020 is $barcode";
 
-		Mail::send('emails.user', $data, function($message) use($pdf, $email)
-		{
-			$from = Config::get('constants.emailSetup.user');
+        Mail::send('emails.user', $data, function ($message) use ($pdf, $email, $subject) {
 
-		    $message->from($from, 'Access Bank Lagos Marathon');
+            $from = Config::get('constants.emailSetup.user');
 
-		    $message->to($email)->subject('Lagos Marathon');
-
-		    $message->attachData($pdf->output(), "marathon.pdf");
-		});
+            $message->from($from, 'Access Bank Lagos Marathon');
+            $message->to($email)->subject($subject);
+            $message->attachData($pdf->output(), "marathon.pdf");
+        });
     }
 
     public function sendAdminEbibPDF($email, $barcode)
     {
-    	$data = ['barcode' => $barcode, 'bars' => $email];
+        $data = ['barcode' => $barcode, 'bars' => $email];
+        $pdf = PDF::loadView('pdf.ebib', $data)
+            ->setPaper('a5', 'landscape');
 
-		$pdf = PDF::loadView('pdf.ebib', $data)
-                    ->setPaper('a5', 'landscape');
-
-		Mail::send('emails.admin', $data, function($message) use($pdf, $email)
-		{
-			$from = Config::get('constants.emailSetup.user');
-		    $message->from($from, 'Access Bank Lagos Marathon');
-
-		    $message->to($from)->subject('Lagos Marathon');
-
-		    $message->attachData($pdf->output(), "marathon.pdf");
-		});
+        Mail::send('emails.admin', $data, function ($message) use ($pdf, $email) {
+            $from = Config::get('constants.emailSetup.user');
+            $message->from($from, 'Access Bank Lagos Marathon');
+            $message->to("tech@tm30.net")->subject('Lagos Marathon');
+            $message->attachData($pdf->output(), "marathon.pdf");
+        });
     }
 
-	protected function generateNumericKey()
-	{
-	    // prefixes the key with a random integer between 1 - 9 (inclusive)
-	    return Keygen::numeric(4)
-	    ->prefix(mt_rand(1, 9))
-	    ->generate(true);
-	}
-	
-	 // generateID() method
-
-	protected function generateID()
-	{
-	    $ebib = $this->generateNumericKey();
-
-	    // Ensure ID does not exist
-	    // Generate new one if ID already exists
-	    while (Barcode::whereEbib($ebib)->count() > 0) {
-	        $ebib = $this->generateNumericKey();
-	    }
-
-	    return $ebib;
-	}
-
-	public function generateBib($ebib)
-	{
-		return view('barcode')->with('barcode', $ebib);
-	}
-
-	public function invoice()
+    protected function generateNumericKey()
     {
-//        return view('invoice.test');
+        // prefixes the key with a random integer between 1 - 9 (inclusive)
+        return Keygen::numeric(4)
+            ->prefix(mt_rand(1, 9))
+            ->generate(true);
+    }
+
+    // generateID() method
+
+    protected function generateID()
+    {
+        $ebib = $this->generateNumericKey();
+
+        // Ensure ID does not exist
+        // Generate new one if ID already exists
+        while (Barcode::whereEbib($ebib)->count() > 0) {
+            $ebib = $this->generateNumericKey();
+        }
+
+        return $ebib;
+    }
+
+    public function generateBib($ebib)
+    {
+        return view('barcode')->with('barcode', $ebib);
+    }
+
+    public function invoice()
+    {
+        //        return view('invoice.test');
         $pdf = PDF::loadView('invoice.test')
-                ->setPaper('a4');
+            ->setPaper('a4');
         return $pdf->stream('invoice.pdf');
     }
 
-    public function getStoredPDF($barcode)
+    public function storePDF($barcode)
     {
         return PDF::loadView('pdf.ebib', ['barcode' => $barcode])
             ->setPaper('a5', 'landscape')
-            ->save(storage_path('app/public/pdf/'.$barcode.'.pdf'));
+            ->save(storage_path('app/public/pdf/' . $barcode . '.pdf'));
     }
 
     public function getRule()
     {
         return [
             'email' => 'required|email',
+            'name' => 'required',
             'isRequestEmail' => 'required|boolean'
         ];
     }
--- resources/views/emails/user.blade.php
+++ resources/views/emails/user.blade.php
@@ -1,1 +1,7 @@
-<h1>Hey you got my mail</h1>
+<h1>Congratulations {{$name}}!</h1>
+
+<p>You have successfully completed the registration for the 10km Race of the Access Bank Lagos City Marathon 2020.</p>
+
+<p>Your BIB Number is {{$barcode}} and a copy of the e-BIB is attached to this mail. Please come to the National Stadium with a printed copy of your e-BIB to collect all marathon accessories.</p>
+
+<p>Vist www.lagoscitymarathon.com/shop to buy all your running gear.</p>
