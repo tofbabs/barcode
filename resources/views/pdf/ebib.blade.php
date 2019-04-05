dc6266ae88eb6329896b0cb48b0897c4aa1b7294
Tofunmi <tofunmibabatunde@gmail.com>
2019-04-05 14:36:35 +0100

    Update User Email

--- resources/views/pdf/ebib.blade.php
+++ resources/views/pdf/ebib.blade.php
@@ -14,7 +14,7 @@
     <div class="top-banner">
         <img src="images/marathon1.png" alt="Marathon">
         <span style="font-size: 180px; font-family: Arial, Helvetica, sans-serif">{{$barcode}}</span>
-        <span style="margin: 20px 0">{!! DNS1D::getBarcodeHTML("$barcode", "C128B"); !!}</span>
+        <span style="bottom: 30%; left: 80%">{!! DNS1D::getBarcodeHTML("$barcode", "C128B"); !!}</span>
     </div>
     <div class="bottom-banner">
         <img src="images/marathon2.png" alt="Marathon">
