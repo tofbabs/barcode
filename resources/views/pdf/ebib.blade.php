6cca9ea38a42ec32a16ac278c8a47cbf67768e24
Tofunmi <tofunmibabatunde@gmail.com>
2019-04-05 16:51:06 +0100

    Updated banner

--- resources/views/pdf/ebib.blade.php
+++ resources/views/pdf/ebib.blade.php
@@ -12,7 +12,7 @@
             text-align: center;
             transform: translate(-50%,-50%);">
     <div class="top-banner">
-        <img src="images/marathon1.png" alt="Marathon">
+        <img width="" src="images/marathon1.png" alt="Marathon">
         <strong style="font-size: 200px; font-family: Arial, Helvetica, sans-serif">{{$barcode}}</strong>
         <div style="position: relative; margin: 20px 0; left: 80%">{!! DNS1D::getBarcodeHTML("$barcode", "C128B"); !!}</div>
     </div>
